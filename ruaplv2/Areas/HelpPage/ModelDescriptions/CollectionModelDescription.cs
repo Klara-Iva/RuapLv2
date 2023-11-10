eb2865a8d1f5b65b565a67903d654d661aff9384
Klara-Iva <klaraiva19@gmail.com>
2023-11-10 14:11:21 +0100

    first commit

---
+++ ruaplv2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ruaplv2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ruaplv2/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using ruaplv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ruaplv2.Services;
+
+namespace ruaplv2.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+            }
+
+    }
+}
